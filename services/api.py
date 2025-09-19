7a051b31b4eb3321198bee3b3d623052022546b1
Abdullah Khetran <khetranabdullah@gmail.com>
2025-09-20 04:47:09 +0500

    added api key to acess backend

--- services/api.py
+++ services/api.py
@@ -4,12 +4,16 @@ import streamlit as st
 
 
 BACKEND_URL = st.secrets["BACKEND_URL"]
+BACKEND_API_KEY = st.secrets["BACKEND_API_KEY"]
 
 
 def get_polarization_score(topic: str) -> Dict[str, Any]:
     payload = {"topic": topic}
+    headers = {"Authorization": BACKEND_API_KEY}
+
     try:
-        response = requests.post(f"{BACKEND_URL}/analyze", json=payload)
+        response = requests.post(
+            f"{BACKEND_URL}/analyze", json=payload, headers=headers)
         if response.status_code == 200:
             return response.json()
         else:
